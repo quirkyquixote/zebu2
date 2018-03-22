931576f8f50d250082e4b4848ee6f86cc501c41b
Luis Sanz <luis.sanz@gmail.com>
2018-03-22 23:58:10 +0100

    Minor renaming of some structs and functions

--- src/zebu.c
+++ src/zebu.c
@@ -3,9 +3,13 @@
 
 const char *ZZ_PAIR = "pair";
 
-struct zz_blob *zz_blob_new(void)
+struct zz_blob {
+        struct zz_blob *next, *prev;
+};
+
+struct zz_blob *zz_blob_new(int size)
 {
-        struct zz_blob *node = malloc(sizeof(*node));
+        struct zz_blob *node = malloc(sizeof(*node) + size);
         node->next = node;
         node->prev = node;
         return node;
@@ -19,11 +23,11 @@ void zz_blob_destroy(struct zz_blob *node)
                 node = next;
         }
 }
-int zz_empty(struct zz_blob *node)
+int zz_blob_empty(struct zz_blob *node)
 {
         return node->next == node;
 }
-void zz_link(struct zz_blob *node, struct zz_blob *next)
+void zz_blob_link(struct zz_blob *node, struct zz_blob *next)
 {
         struct zz_blob *prev = next->prev;
         node->next = next;
@@ -31,7 +35,7 @@ void zz_link(struct zz_blob *node, struct zz_blob *next)
         prev->next = node;
         next->prev = node;
 }
-void zz_unlink(struct zz_blob *node)
+void zz_blob_unlink(struct zz_blob *node)
 {
         node->next->prev = node->prev;
         node->prev->next = node->next;
@@ -39,51 +43,51 @@ void zz_unlink(struct zz_blob *node)
 
 void zz_ast_init(struct zz_ast* a)
 {
-        a->blobs = zz_blob_new();
+        a->blobs = zz_blob_new(0);
 }
 void zz_ast_deinit(struct zz_ast *a)
 {
         zz_blob_destroy(a->blobs);
 }
-void *zz_node_new(struct zz_ast* a, int size)
+void *zz_ast_alloc(struct zz_ast* a, int size)
 {
-        struct zz_blob *b = malloc(sizeof(*b) + size);
-        zz_link(b, a->blobs);
+        struct zz_blob *b = zz_blob_new(size);
+        zz_blob_link(b, a->blobs);
         return (void *)(b + 1);
 }
 void zz_ast_gc(struct zz_ast *a, struct zz_node *root)
 {
-        if (zz_empty(a->blobs))
+        if (zz_blob_empty(a->blobs))
                 return;
 
         struct zz_blob *white = a->blobs;
-        struct zz_blob *gray = zz_blob_new();
-        struct zz_blob *black = zz_blob_new();
+        struct zz_blob *gray = zz_blob_new(0);
+        struct zz_blob *black = zz_blob_new(0);
 
         {
                 struct zz_blob *b = (struct zz_blob *)root - 1;
-                zz_unlink(b);
-                zz_link(b, gray);
+                zz_blob_unlink(b);
+                zz_blob_link(b, gray);
         }
 
-        while (!zz_empty(gray)) {
+        while (!zz_blob_empty(gray)) {
                 struct zz_blob *b = gray->next;
-                zz_unlink(b);
+                zz_blob_unlink(b);
                 struct zz_node *n = (void *)(b + 1);
                 if (zz_is_pair(n)) {
                         struct zz_pair *p = zz_pair(n);
                         if (p->data) {
                                 struct zz_blob *c = (struct zz_blob *)p->data - 1;
-                                zz_unlink(c);
-                                zz_link(c, gray);
+                                zz_blob_unlink(c);
+                                zz_blob_link(c, gray);
                         }
                         if (p->next) {
                                 struct zz_blob *c = (struct zz_blob *)p->next - 1;
-                                zz_unlink(c);
-                                zz_link(c, gray);
+                                zz_blob_unlink(c);
+                                zz_blob_link(c, gray);
                         }
                 }
-                zz_link(b, black);
+                zz_blob_link(b, black);
         }
 
         zz_blob_destroy(white);
@@ -94,7 +98,7 @@ void zz_ast_gc(struct zz_ast *a, struct zz_node *root)
 struct zz_node *zz_pair_new(struct zz_ast *a,
                 struct zz_node *data, struct zz_node *next)
 {
-        struct zz_pair *n = zz_node_new(a, sizeof(*n));
+        struct zz_pair *n = zz_ast_alloc(a, sizeof(*n));
         n->type = ZZ_PAIR;
         n->data = data;
         n->next = next;
@@ -112,7 +116,7 @@ struct zz_pair *zz_pair(struct zz_node *n)
 struct zz_node *zz_atom_new_with_len(struct zz_ast *a,
                 const char *type, const char *str, int len)
 {
-        struct zz_atom *n = zz_node_new(a, sizeof(*n) + len + 1);
+        struct zz_atom *n = zz_ast_alloc(a, sizeof(*n) + len + 1);
         n->type = type;
         memcpy(n->str, str, len);
         n->str[len] = 0;
--- src/zebu.h
+++ src/zebu.h
@@ -8,13 +8,20 @@
 
 extern const char *ZZ_PAIR;
 
-struct zz_blob {
-        struct zz_blob *next, *prev;
-};
-
 struct zz_node {
         const char* type;
 };
+
+int zz_print(struct zz_node *n, FILE *f);
+
+struct zz_ast {
+        void *blobs;
+};
+
+void zz_ast_init(struct zz_ast* a);
+void zz_ast_deinit(struct zz_ast *a);
+void *zz_ast_alloc(struct zz_ast* a, int size);
+void zz_ast_gc(struct zz_ast *a, struct zz_node *root);
 
 struct zz_pair {
         const char *type;
@@ -22,37 +29,30 @@ struct zz_pair {
         struct zz_node* next;
 };
 
-struct zz_atom {
-        const char *type;
-        char str[];
-};
-
-struct zz_ast {
-        struct zz_blob *blobs;
-};
-
-void zz_ast_init(struct zz_ast* a);
-void zz_ast_deinit(struct zz_ast *a);
-void *zz_node_new(struct zz_ast* a, int size);
-void zz_ast_gc(struct zz_ast *a, struct zz_node *root);
-
-struct zz_node *zz_pair_new(struct zz_ast *a, struct zz_node *data, struct zz_node *next);
+struct zz_node *zz_pair_new(struct zz_ast *a, struct zz_node *data,
+                struct zz_node *next);
 int zz_is_pair(struct zz_node *n);
 struct zz_pair *zz_pair(struct zz_node *n);
-
-struct zz_node *zz_atom_new_with_len(struct zz_ast *a, const char *type, const char *str, int len);
-struct zz_node *zz_atom_new(struct zz_ast *a, const char *type, const char *str);
-int zz_is_atom(struct zz_node *n);
-struct zz_atom *zz_atom(struct zz_node *n);
 
 #define zz_foreach(_x, _head) \
 for (struct zz_pair* _i = zz_pair(_head); \
                 _x = _i ? _i->data : NULL, _i; \
                 _i = zz_pair(_i->next))
 
+struct zz_atom {
+        const char *type;
+        char str[];
+};
+
+struct zz_node *zz_atom_new_with_len(struct zz_ast *a, const char *type,
+                const char *str, int len);
+struct zz_node *zz_atom_new(struct zz_ast *a, const char *type,
+                const char *str);
+int zz_is_atom(struct zz_node *n);
+struct zz_atom *zz_atom(struct zz_node *n);
+
 struct zz_node *zz_list_append(struct zz_ast *a, struct zz_node *head, struct zz_node *x);
 struct zz_node *zz_list_prepend(struct zz_ast *a, struct zz_node *head, struct zz_node *x);
 
-int zz_print(struct zz_node *n, FILE *f);
 
 #endif  // _ZEBU_H
